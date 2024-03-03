122d21bc5bd1c3516cf684d008186761108309eb
jay zadafiya <zadafiyajay2@gmail.com>
2024-03-03 14:57:40 +0530

    Setting security Http Header and done data sanitization

--- app.js
+++ app.js
@@ -1,42 +1,48 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
+//set security HTTP headers
+app.use(helmet());
 
 // 1) MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+//set rate limit
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
   message: 'Too many reuest for this Ip, please try again in a hour'
 });
-
 //limiter affect only all /api routee
 app.use('/api', limiter);
 
-app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
+//Body parser, reading data from body int req.body
+app.use(express.json({ limit: '10kb' }));
 
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
-});
+//data sanitization aginest NoSQL query injection
+app.use(mongoSanitize());
+
+//data sanitization aginest xss=html code with js convert to entity
+app.use(xss());
+
+//serving static file
+app.use(express.static(`${__dirname}/public`));
 
 // 3) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   message: `Cant't find ${req.originalUrl} on this server!`
-  // });
   const err = new Error(`Cant't find ${req.originalUrl} on this server!`);
   err.status = 'Fail';
   err.statusCode = 400;
